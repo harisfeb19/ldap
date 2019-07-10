e40e5e3899cb72882f824e496a6c36ca17b20fcd
harisfeb19 <harisfeb19.gmail.com>
2019-07-10 16:33:21 +0500

    Commit

---
+++ src/main/java/com/opencodez/adldapdemo/controllers/LoginController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.opencodez.adldapdemo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+
+	@RequestMapping(value = "login", method = RequestMethod.GET)
+	public ModelAndView Login() {
+		return new ModelAndView("login");
+	}
+}
